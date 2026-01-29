053494c36151f39a7b587b624ad8510d57c4a48c
delacruzquintanaregina04 <delacruzquintanaregina@gmail.com>
2026-01-29 00:23:51 -0600

    Feat: Se agrega html y java(consola)

---
+++ main.js
@@ -0,0 +1,54 @@
+//Comentario en JavaScript
+
+//Tipos de datos
+
+//Tipo de dato: Number
+
+let edad = 24; //Variable de tipo número(guarda información)
+let precio = 199.99; //Variable de tipo número con decimales
+console.log("Number:", edad); //Imprime la edad en la consola
+console.log(precio);
+console.log(typeof precio); //Muestra el tipo de dato de la variable precio
+
+//Tipo de dato: String
+
+let nombre = "Regina"; //Variable de tipo cadena de texto
+let saludo = 'Hola, ¿cómo estás?'; //Variable de tipo cadena de texto con comillas simples
+console.log("tipo de dato");
+console.log(typeof nombre); //Muestra el tipo de dato de la variable nombre
+
+//Tipo de dato: Boolean
+let esEstudiante = true; //Variable de tipo booleano
+let tieneTrabajo = false; //Variable de tipo booleano
+console.log("Tipo de dato Boolean:");
+console.log(typeof esEstudiante); //Muestra el tipo de dato de la variable esEstudiante
+console.log(esEstudiante);
+console.log(tieneTrabajo);
+console.log(typeof tieneTrabajo); //Muestra el tipo de dato de la variable tieneTrabajo
+
+//Tipo de dato: Undefined
+let direccion;
+console.log("Tipo de dato Undefined:");
+console.log(direccion);
+
+//Tipo de dato: Null
+let telefono = null; //Variable de tipo nulo
+console.log("Tipo de dato Null:");
+console.log(telefono);
+console.log(typeof telefono); //Muestra el tipo de dato de la variable telefono
+
+//Tipo de dato: Object (estructura de datos con propiedades)
+let persona = {
+    nombre: "Regina",
+    edad: "24",
+    ciudad: "Ciudad de México"
+};
+console.log("Tipo de dato Object:");
+console.log(typeof persona); //Muestra el tipo de dato de la variable persona
+console.log(persona);
+
+//Tipo de dato: simbol (identificador único
+let id= Symbol("id");
+console.log("Tipo de dato Symbol:");
+console.log(id);
+console.log(typeof id);
