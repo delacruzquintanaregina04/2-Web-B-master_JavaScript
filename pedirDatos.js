0558b01223089dee916f6b4b53957a9e8e91cc26
delacruzquintanaregina04 <delacruzquintanaregina@gmail.com>
2026-01-29 19:52:05 -0600

    Feat: "Variables y operadores"

---
+++ pedirDatos.js
@@ -0,0 +1,11 @@
+console.log("Hola me llamo regina");
+
+console.log("Como te llamas?");
+
+let nombreUsuario = prompt("Por favor ingresa tu nombre:");
+//console.log(nombreUsuario);
+//document.write(nombreUsuario);
+alert(nombreUsuario);
+
+let fecha = prompt("Cuando es nu cumpleaños?");
+console.log(fecha);
